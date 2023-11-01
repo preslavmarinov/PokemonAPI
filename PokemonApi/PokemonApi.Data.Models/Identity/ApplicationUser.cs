9b87bc779e4c13f7aa89d1b990ffc41aa9c2df26
preslavmarinov <preslavivanovmarinov@gmail.com>
2023-11-01 19:23:28 +0200

    Project setup

---
+++ PokemonApi/PokemonApi.Data.Models/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonApi.Data.Models.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ICollection<PokemonEntity> Pokemons { get; set; } = new HashSet<PokemonEntity>();
+    }
+}
---
+++ PokemonApi/PokemonApi.Data/PokemonDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PokemonApi.Data.Models;
+using PokemonApi.Data.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonApi.Data
+{
+    public class PokemonDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public PokemonDbContext(DbContextOptions<PokemonDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<PokemonEntity> Pokemons { get; set; } = default!;
+
+        public DbSet<LocationEntity> Location { get; set; } = default!;
+
+        public DbSet<TypeEntity> Type { get; set; } = default!;
+    }
+}
